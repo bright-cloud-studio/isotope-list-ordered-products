ec42faa87edf4f846d92eaa1b5f31a80e7d9ef19
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-24 12:34:42 -0400

    Create tl_module.php

---
+++ system/modules/isotope_list_ordered_products/dca/tl_module.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+// Palette for our module
+$GLOBALS['TL_DCA']['tl_module']['palettes']['iso_list_ordered_products'] = '{title_legend},name,headline,type;{config_legend},iso_config_ids;{redirect_legend},jumpTo,iso_cart_jumpTo;{template_legend},customTpl,iso_includeMessages;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
