e6b0d32842853d41aaa42a3a1e60280867ba7ee5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-24 12:32:12 -0400

    Create config.php

---
+++ system/modules/isotope_list_ordered_products/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+
+// Frontend Modules
+$GLOBALS['FE_MOD']['isotope']['iso_list_ordered_products'] 	= 'Bcs\Module\ListOrderedProducts.php';
