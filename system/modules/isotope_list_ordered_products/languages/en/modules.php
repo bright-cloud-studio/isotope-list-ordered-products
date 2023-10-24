d59ad697ab406da01a9678ac70223c940e3da04f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-24 12:36:32 -0400

    Create modules.php

---
+++ system/modules/isotope_list_ordered_products/languages/en/modules.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+
+// Front End Module
+$GLOBALS['TL_LANG']['FMD']['iso_list_ordered_products'] = array('List Ordered Products', 'A customized Isotope Order History module to list ordered products as a Product List module');
