197414356ff34f80459b0ac59ed03d9b6b8f37e1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-24 12:38:55 -0400

    Create ListOrderedProducts.php

---
+++ system/modules/isotope_list_ordered_products/library/Bcs/Module/ListOrderedProducts.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+  
+namespace Bcs\Module;
+
+use Isotope\Module\OrderHistory;
+
+class ListOrderedProducts extends OrderHistory
+{
+
+}
