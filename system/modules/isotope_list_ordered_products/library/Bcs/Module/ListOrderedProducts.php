027b783fa5eefbaee86789b62bed52ca31bd3bf3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-27 10:32:53 -0400

    Update ListOrderedProducts.php

--- system/modules/isotope_list_ordered_products/library/Bcs/Module/ListOrderedProducts.php
+++ system/modules/isotope_list_ordered_products/library/Bcs/Module/ListOrderedProducts.php
@@ -14,6 +14,9 @@
 namespace Bcs\Module;
 
 use Isotope\Module\ProductList;
+use Isotope\Model\ProductCollectionItem;
+use Isotope\Model\ProductCollection\Order;
+use Isotope\Interfaces\IsotopeProduct;
 
 use Contao\FrontendUser;
 
@@ -21,12 +24,48 @@ class ListOrderedProducts extends ProductList
 {
     // Template
     protected $strTemplate = 'mod_iso_list_ordered_products';
+    
+    protected function compile() {
+        //parent::compile();
+        //$this->Template->test_2 = $this->findProducts();
+    }
 
     protected function findProducts($arrCacheIds = null)
     {
+        
+        // Get our front end user
         $objUser = FrontendUser::getInstance();
+        
+        // If we dont have a user, return
+        if (!$objUser) {
+			return [];
+		}
 
         $arrProducts = [];
+		$arrIds = [];
+		
+		$arrFind = array(
+			'column' 	=> array("member=?", "document_number!=''"),
+			'value'		=> $objUser->id
+		);
+		
+		$objOrder = Order::findAll($arrFind);
+		
+		return $objOrder;
+		
+		if ($objOrder) {
+			while($objOrder->next) {
+				$objItem = ProductCollectionItem::findBy('pid', $objOrder->id);
+				if ($objItem) {
+					while ($objItem->next()) {
+						if (!in_array($objItem->product_id, $arrIds)) {
+							$arrIds[] = $objItem->product_id;
+							$arrProducts[] = $objItem->current()->getProduct();
+						}
+					}
+				}
+			}
+		}
 
         return $arrProducts;
     }
