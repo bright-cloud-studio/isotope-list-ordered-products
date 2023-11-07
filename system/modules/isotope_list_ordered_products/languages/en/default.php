0a688d060bf29964e72703bdf246c7be323c58e4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-07 13:54:21 -0500

    Create default.php

---
+++ system/modules/isotope_list_ordered_products/languages/en/default.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope List Ordered Products
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-list-ordered-products
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+ 
+ 
+
+$GLOBALS['TL_LANG']['MSC']['addedToCartBatch'] 	= 'The selected products have been added to your shopping cart.';
