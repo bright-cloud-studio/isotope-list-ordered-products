440727e05e153b4d0d535a155f5f789aeb3937c7
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-10 08:11:43 -0500

    Update modules.php

--- system/modules/isotope_list_ordered_products/languages/en/modules.php
+++ system/modules/isotope_list_ordered_products/languages/en/modules.php
@@ -15,3 +15,4 @@
 // Front End Module
 $GLOBALS['TL_LANG']['FMD']['iso_list_ordered_products']    = array('List Ordered Products', 'A customized Isotope Order History module to list ordered products as a Product List module');
 $GLOBALS['TL_LANG']['tl_module']['enableBatchAdd']         = array('Enable Batch Product Add To Cart','');
+$GLOBALS['TL_LANG']['tl_module']['enableOrderedFilter']    = array('Enable Filtering to Previously Ordered Product only','');
