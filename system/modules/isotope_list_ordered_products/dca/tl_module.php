8ddfa0aa0d3bc5d06f6d53832c4ec55dbca280a2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-11-10 08:02:06 -0500

    Update tl_module.php

--- system/modules/isotope_list_ordered_products/dca/tl_module.php
+++ system/modules/isotope_list_ordered_products/dca/tl_module.php
@@ -12,10 +12,18 @@
 
 
 // Palette for our module
-$GLOBALS['TL_DCA']['tl_module']['palettes']['iso_list_ordered_products'] = '{title_legend},name,headline,type;{config_legend},numberOfItems,perPage,iso_listingSortField,iso_listingSortDirection,enableBatchAdd;{redirect_legend},iso_link_primary,iso_jump_first,iso_addProductJumpTo,iso_wishlistJumpTo,moss_shareProtocolJumpTo,moss_deleteProtocolJumpTo,moss_shareSavedListJumpTo,moss_deleteSavedListJumpTo;{reference_legend:hide},defineRoot;{template_legend:hide},customTpl,iso_list_layout,iso_gallery,iso_cols,iso_use_quantity,iso_hide_list,iso_disable_options,iso_includeMessages,iso_emptyMessage,iso_emptyFilter,iso_buttons;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['iso_list_ordered_products'] = '{title_legend},name,headline,type;{config_legend},numberOfItems,perPage,iso_listingSortField,iso_listingSortDirection,enableBatchAdd,enableOrderedFilter;{redirect_legend},iso_link_primary,iso_jump_first,iso_addProductJumpTo,iso_wishlistJumpTo,moss_shareProtocolJumpTo,moss_deleteProtocolJumpTo,moss_shareSavedListJumpTo,moss_deleteSavedListJumpTo;{reference_legend:hide},defineRoot;{template_legend:hide},customTpl,iso_list_layout,iso_gallery,iso_cols,iso_use_quantity,iso_hide_list,iso_disable_options,iso_includeMessages,iso_emptyMessage,iso_emptyFilter,iso_buttons;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['fields']['enableBatchAdd'] = array
 (
     'label'                     => &$GLOBALS['TL_LANG']['tl_module']['enableBatchAdd'],
+    'exclude'                   => true,
+    'inputType'                 => 'checkbox',
+    'eval'                      => array('tl_class'=>'w50'),
+    'sql'                       => "char(1) NOT NULL default ''",
+);
+$GLOBALS['TL_DCA']['tl_module']['fields']['enableOrderedFilter'] = array
+(
+    'label'                     => &$GLOBALS['TL_LANG']['tl_module']['enableOrderedFilter'],
     'exclude'                   => true,
     'inputType'                 => 'checkbox',
     'eval'                      => array('tl_class'=>'w50'),
