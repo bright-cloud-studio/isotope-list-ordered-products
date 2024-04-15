7b3719e1359f428e0ab1ae3533e5752e4066ff47
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-15 14:25:51 -0400

    Update OrderedProductsFrontend.php

--- system/modules/isotope_list_ordered_products/library/Bcs/Frontend/OrderedProductsFrontend.php
+++ system/modules/isotope_list_ordered_products/library/Bcs/Frontend/OrderedProductsFrontend.php
@@ -24,13 +24,8 @@ class OrderedProductsFrontend extends \Isotope\Frontend
 {
 
     /* Called when clicking the "Add to Cart" button */
-    public function addToCartBatch($objModule, array $arrConfig = array())
+    public function addToCartBatch(&$objModule, array $arrConfig = array())
     {
-
-
-        $objModule->Template->testy = "ding";
-        die();
-        
         
         /* If we have CSV data in our form */
         if (\Input::post('csv_data') != "")
