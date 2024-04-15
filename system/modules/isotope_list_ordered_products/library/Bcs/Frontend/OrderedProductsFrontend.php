e1d4d6ae03f72c86fa8accb713f8e00ab34df9d5
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-15 15:42:42 -0400

    Update OrderedProductsFrontend.php

--- system/modules/isotope_list_ordered_products/library/Bcs/Frontend/OrderedProductsFrontend.php
+++ system/modules/isotope_list_ordered_products/library/Bcs/Frontend/OrderedProductsFrontend.php
@@ -30,7 +30,7 @@ class OrderedProductsFrontend extends \Isotope\Frontend
         //$objModule = null;
 
         $arr2 = array("test_1", "test_2");
-        array_splice( $objModule, 1, 0, $arr2 ); 
+        array_splice( &$objModule, 1, 0, $arr2 ); 
         
         /* If we have CSV data in our form */
         if (\Input::post('csv_data') != "")
