dc048320128a92b36973353e83be966798993b9f
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-17 13:09:02 -0400

    Update Handler.php

--- system/modules/isotope_list_ordered_products/library/Bcs/Handler.php
+++ system/modules/isotope_list_ordered_products/library/Bcs/Handler.php
@@ -22,7 +22,6 @@ class Handler
 
             /* Files is not null, we have an upload submission */
             if($files != null) {
-                echo "FILE FOUND<br>";
                 
                 // Get the Contao file
                 $csv = \FilesModel::findByUuid($files['csv_upload']['uuid']);
@@ -35,12 +34,26 @@ class Handler
                 $str_csv = fgetcsv($file);
                 // Break that array into chunks of 2 (sku,quantity)
                 $chunks = array_chunk($str_csv, 2);
-                
-                echo "<pre>";
-                print_r($chunks);
-                echo "</pre>";
-                
-                die();
+
+                // Loop through our array csv array
+                foreach($chunks as $prod)
+                {
+                    /* If the quantity is entered as 0, coninue on */
+                    if(intval($prod[1])==0)
+        			    continue;
+    
+                    /* Find product by SKU */
+                    $objProd = Product::findOneBy(['tl_iso_product.sku=?'],[$prod[0]]);
+
+                    /* If we found a product */
+                    if($objProd != null) {
+                        
+                        // If there is no error after adding this product to the cart
+                        $arrConfig = array();
+                        if (Isotope::getCart()->addProduct($objProd, $prod[1], $arrConfig) !== false)
+                            $blnAdded = true;
+                    }
+                }
                 
             } else if($submittedData['csv_string'] != "") {
 
@@ -55,7 +68,6 @@ class Handler
                 // Loop through our array csv array
                 foreach($chunks as $prod)
                 {
-    
                     /* If the quantity is entered as 0, coninue on */
                     if(intval($prod[1])==0)
         			    continue;
@@ -63,12 +75,6 @@ class Handler
                     /* Find product by SKU */
                     $objProd = Product::findOneBy(['tl_iso_product.sku=?'],[$prod[0]]);
 
-                    //echo "<pre>";
-                    //print_r($objProd);
-                    //echo "</pre>";
-                    //die();
-                    
-                    
                     /* If we found a product */
                     if($objProd != null) {
                         
@@ -77,16 +83,16 @@ class Handler
                         if (Isotope::getCart()->addProduct($objProd, $prod[1], $arrConfig) !== false)
                             $blnAdded = true;
                     }
-                    
-                }
-    
-                /* If we have added a product to the cart */
-                if($blnAdded) {
-                    $_SESSION['ISO_CONFIRM'][] = $GLOBALS['TL_LANG']['MSC']['addedToCartBatch'];
-                    \Controller::redirect(\Haste\Util\Url::addQueryString('continue=' . base64_encode(\Environment::get('request')), $objModule->iso_addProductJumpTo));
                 }
 
             }
+
+            /* If we have added a product to the cart */
+            if($blnAdded) {
+                $_SESSION['ISO_CONFIRM'][] = $GLOBALS['TL_LANG']['MSC']['addedToCartBatch'];
+                \Controller::redirect(\Haste\Util\Url::addQueryString('continue=' . base64_encode(\Environment::get('request')), $objModule->iso_addProductJumpTo));
+            }
+            
 
         }
 
